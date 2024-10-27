ff367b743528283812d06cf0df85f8cd6df0e70a
Joseph Lu <josephlu414@gmail.com>
2024-10-27 01:51:39 -0400

    Added env file to docker run script

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name court-iq-container --ipc host --gpus all --rm -p 8888:8888 -it -v ./:/root/ court-iq
+docker run --name court-iq-container --ipc host --env-file .env --gpus all --rm -p 8888:8888 -it -v ./:/root/ court-iq
