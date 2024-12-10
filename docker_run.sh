d40cc3b2eaab65a2a9ac6140b4c38b439fca947e
Joseph Lu <josephlu414@gmail.com>
2024-12-09 21:42:24 -0500

    fixed docker run

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --runtime=nvidia --name court-iq-container --ipc host --env-file .env --gpus all --rm -p 8080:8888 -it -v ./:/root/ court-iq
+docker run --runtime=nvidia --name court-iq-container --ipc host --env-file .env --gpus all --rm -p 8888:8888 -it -v ./:/root/ court-iq
